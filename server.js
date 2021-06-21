3376b106574d47d4d2df618bbaef424d2c38759a
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-06-21 16:40:12 +0300

    seed database

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
-const express = require('express') // require the express package
-const app = express() // initialize your express app instance
+const express = require('express'); // require the express package
+const app = express(); // initialize your express app instance
 require('dotenv').config();
 const PORT = process.env.PORT;
 const cors = require('cors');
-
 const axios = require('axios'); // require the package
+const seedUserData = require('./models/user.model');
+const getBooks = require('./controller/book.controller');
+app.use(cors()); // after you initialize your express app instance
+// a server endpoint
+app.get(
+  '/', // our endpoint name
+  function (req, res) {
+    // callback function of what we should do with our request
+    res.send('TEST');
 
-app.use(cors()) // after you initialize your express app instance
-// a server endpoint 
-app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('TEST')
-  
-  axios.get(url).then(response => response.data).catch(error => console.log(error));
-  // our endpoint function response
-})
- 
+    axios
+      .get(url)
+      .then((response) => response.data)
+      .catch((error) => console.log(error));
+    // our endpoint function response
+  }
+);
 
-app.get('/test', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('proof of live')
-  
-  axios.get(url).then(response => response.data).catch(error => console.log(error));
-  // our endpoint function response
-})
-app.listen(PORT) // kick start the express server to work
+app.get(
+  '/test', // our endpoint name
+  function (req, res) {
+    // callback function of what we should do with our request
+    res.send('proof of live');
+
+    axios
+      .get(url)
+      .then((response) => response.data)
+      .catch((error) => console.log(error));
+    // our endpoint function response
+  }
+);
+
+app.get('/books', getBooks);
+
+seedUserData();
+app.listen(PORT); // kick start the express server to work
